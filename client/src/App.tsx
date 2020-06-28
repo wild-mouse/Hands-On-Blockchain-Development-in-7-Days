8af8f1a9fbee134dea167bad276e6a083a40fe21
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-06-28 16:32:23 +0900

    Add simple page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+} from '@material-ui/core'
 
 function App() {
+  const [playerNumber, setPlayerNumber] = useState<number>(1)
+  const [highLow, setHighLow] = useState<boolean>(true)
+  const [wager, setWager] = useState<number>(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <CardHeader
+          title="Player Number"
+          subheader="Will the mystery number be higher or lower than this number?"
+        />
+        <CardContent>1</CardContent>
+      </Card>
+      <Card>
+        <CardHeader title="Betting Window" />
+        <CardContent>
+          <FormControl component="fieldset">
+            <RadioGroup
+              aria-label="highLow"
+              name="highLow"
+              value={highLow}
+              onChange={(e) => setHighLow(e.target.value === 'true')}
+            >
+              <FormControlLabel
+                value={true}
+                control={<Radio />}
+                label="Higher"
+              />
+              <FormControlLabel
+                value={false}
+                control={<Radio />}
+                label="Lower"
+              />
+            </RadioGroup>
+            <Input
+              value={wager}
+              type="number"
+              onChange={(e) => setWager(Number.parseFloat(e.target.value))}
+            />
+          </FormControl>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader title="History" />
+      </Card>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
